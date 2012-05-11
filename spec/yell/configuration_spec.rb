3ac8839cb2032e3c5ac7c368b64607bf845c11ce
Rudolf Schmidt <me@rudionrails.com>
2012-05-11 13:35:39 +0200

    CHORE: Updated configuration spec

--- spec/yell/configuration_spec.rb
+++ spec/yell/configuration_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 describe Yell::Configuration do
 
   describe ":load!" do
-    let( :file ) { File.expand_path File.dirname(__FILE__) + '/../fixtures/yell.yml' }
+    let( :file ) { File.dirname(__FILE__) + '/../fixtures/yell.yml' }
     let( :configuration ) { Yell::Configuration.load!( file ) }
 
     it "should return the hash from the given file" do
-      YAML.load( File.read(file) )['development'].should == configuration
+      YAML.load_file( file )['development'].should == configuration
     end
   end
 
