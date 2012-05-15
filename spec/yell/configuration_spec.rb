98fccb408bee52f231e2e2d6d0162031675fb63b
Rudolf Schmidt <me@rudionrails.com>
2012-05-15 22:37:00 +0200

    IX: Updated configuration specs

--- spec/yell/configuration_spec.rb
+++ spec/yell/configuration_spec.rb
@@ -4,31 +4,25 @@ describe Yell::Configuration do
 
   describe ":load!" do
     let( :file ) { fixture_path + '/yell.yml' }
-    let( :config ) { Yell::Configuration.load!( file ) }
+    let( :configuration ) { Yell::Configuration.load!( file ) }
 
-    subject { config }
+    it { configuration.should be_kind_of Hash }
+    it { configuration.should have_key :level }
+    it { configuration.should have_key :adapters }
 
-    it { should be_kind_of Hash }
-    it { should have_key :level }
-    it { should have_key :adapters }
-
-    context :level do
-      subject { config[:level] }
-
-      it { should == "info" }
+    it "should set the correct :level" do
+      configuration[:level].should == "info"
     end
 
-    context :adapters do
-      subject { config[:adapters] }
-
-      it { should be_kind_of Array }
+    it "should set the correct :adapters" do
+      configuration[:adapters].should be_kind_of Array
 
       # stdout
-      it { subject.first.should == :stdout }
+      configuration[:adapters][0].should == :stdout
 
       # stderr
-      it { subject.last.should be_kind_of Hash }
-      it { subject.last.should == { :stderr => {:level => 'gte.error'} } }
+      configuration[:adapters][1].should be_kind_of Hash
+      configuration[:adapters][1].should == { :stderr => {:level => 'gte.error'} }
     end
   end
 
