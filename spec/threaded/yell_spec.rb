ffd5762b3f3cc88577dccf14691e6f5de6334eba
Rudolf Schmidt <me@rudionrails.com>
2012-09-04 19:13:05 +0200

    CHORE: Added Yell multi-threading specs

---
+++ spec/threaded/yell_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe "running Yell multi-threaded" do
+  let( :threads ) { 100 }
+
+  let( :filename ) { fixture_path + '/threaded.log' }
+  let( :lines ) { `wc -l #{filename}`.to_i }
+
+  it "should write all messages from one instance" do
+    logger = Yell.new( filename )
+
+    (1..threads).map do |count|
+      Thread.new { 10.times { logger.info count } }
+    end.each(&:join)
+
+    lines.should == 10*threads
+  end
+
+  # it "should write all messages from multiple instances" do
+  #   (1..threads).map do |count|
+  #     logger = Yell.new( filename )
+
+  #     Thread.new do
+  #       10.times { logger.info count }
+  #     end
+  #   end.each(&:join)
+
+  #   lines.should == 10*threads
+  # end
+
+  it "should write all messages from one repository" do
+    Yell[ 'threaded' ] = Yell.new( filename )
+
+    (1..threads).map do |count|
+      Thread.new { 10.times { Yell['threaded'].info count } }
+    end.each(&:join)
+
+    lines.should == 10*threads
+  end
+
+end
