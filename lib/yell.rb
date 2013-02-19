e1a93619c8cbe57f2d224f48544ed92a6c21fb6e
Rudolf Schmidt <me@rudionrails.com>
2013-02-19 15:41:04 +0100

    CHORE: Updated License information

--- lib/yell.rb
+++ lib/yell.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-# Copyright (c) 2011-2012 Rudolf Schmidt
+# Copyright (c) 2011-2013 Rudolf Schmidt
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
