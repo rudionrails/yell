c835464096ce2b210e9f74063fda91b16c252552
Chris Beer <cabeer@stanford.edu>
2019-05-15 11:31:46 -0700

    Support ruby 2.4+ style logger levels specified using strings or symbols

--- spec/compatibility/level_spec.rb
+++ spec/compatibility/level_spec.rb
@@ -2,17 +2,35 @@ require 'spec_helper'
 require 'logger'
 
 describe "backwards compatible level" do
-
-  let(:level) { Yell::Level.new(:error) }
   let(:logger) { Logger.new($stdout) }
 
   before do
     logger.level = level
   end
 
-  it "should format out the level correctly" do
-    expect(logger.level).to eq(level.to_i)
+  context "with a Yell::Level instance" do
+    let(:level) { Yell::Level.new(:error) }
+
+    it "should format out the level correctly" do
+      expect(logger.level).to eq(level.to_i)
+    end
   end
 
-end
+  context "with a symbol", :pending => (RUBY_VERSION < "2.3") do
+    let(:level) { :error }
 
+    it "should format out the level correctly" do
+      expect(logger.level).to eq(3)
+    end
+  end
+
+  context "with an integer" do
+    let(:level) { 2 }
+
+    it "should format out the level correctly" do
+      expect(logger.level).to eq(2)
+    end
+  end
+
+
+end
